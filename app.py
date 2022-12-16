5b946d23324c1bfa1dab46ac156850d1ee75f876
Bhavy Bansal <76460299+BhavyBansal24@users.noreply.github.com>
2022-12-16 16:56:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ dataset = dataset.drop(['Serial Num'],axis=1)
 X = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,4].values
 columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(), [0])], remainder='passthrough')
-X = np.array(columnTransformer.fit_transform(X),dtype=np.str)
-X = X[:, 1:] #Prevent Dummy Variable Trap
+X = np.array(columnTransformer.fit_transform(X))
+# X = X[:, 1:] #Prevent Dummy Variable Trap
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.1, random_state = 0)
     
 #Linear Regression
@@ -47,10 +47,10 @@ rfY_pred = rfRegressor.predict(X_test)
 rfAccuracy = rfRegressor.score(X_test,y_test)
 
 st.title("Find the lag")
-options = st.radio("Navigation", ("Dataset", "Lag Prediction Results"), horizontal = True)
+options = st.radio("Navigation", ("Dataset", "Lag Prediction Results", "User Values"), horizontal = True)
 if options == "Dataset":
     df = pd.read_csv("Dataset.csv")
-    st.write(df)
+    st.write(dataset)
 if options == "Lag Prediction Results":
     models = st.selectbox("Select Model", ("Linear Regression", "Random Forest Regression"))
     if models == "Linear Regression":
@@ -59,3 +59,44 @@ if options == "Lag Prediction Results":
     if models == "Random Forest Regression":
         st.write("Accuracy: ", rfAccuracy)
         st.write("MSE: ", float("{:.6f}".format(mean_squared_error(y_test, rfY_pred))))
+if options == "User Values":
+    start_frame = st.text_input("Start Frame", 0)
+    end_frame = st.text_input("End Frame", 0)
+    start_frame = int(start_frame)
+    end_frame = int(end_frame)
+    lag = end_frame - start_frame
+    ftd_ = (end_frame - start_frame) * (1000/240)
+    ftd = "Frame Time Difference : " + str(ftd_)
+    fd = "Frame Difference : " + str(end_frame - start_frame)
+    action = ["ADS HG", "Camera Right", "Crouch", "Melee", "Prone", "Reload HG", "Shoot AR", "Shoot HG", "Shoot LMG", "Walk Back"]
+    video = st.selectbox("Select video", (action))
+    if video == "ADS HG":
+        X = [1,0,0,0,0,0,0,0,0,0,start_frame,end_frame,lag]
+    if video == "Camera Right":
+        X = [0,1,0,0,0,0,0,0,0,0,start_frame,end_frame,lag]
+    if video == "Crouch":
+        X = [0,0,1,0,0,0,0,0,0,0,start_frame,end_frame,lag]
+    if video == "Melee":
+        X = [0,0,0,1,0,0,0,0,0,0,start_frame,end_frame,lag]
+    if video == "Prone":
+        X = [0,0,0,0,1,0,0,0,0,0,start_frame,end_frame,lag]
+    if video == "Reload HG":
+        X = [0,0,0,0,0,1,0,0,0,0,start_frame,end_frame,lag]
+    if video == "Shoot AR":
+        X = [0,0,0,0,0,0,1,0,0,0,start_frame,end_frame,lag]
+    if video == "Shoot HG":
+        X = [0,0,0,0,0,0,0,1,0,0,start_frame,end_frame,lag]
+    if video == "Shoot LMG":
+        X = [0,0,0,0,0,0,0,0,1,0,start_frame,end_frame,lag]
+    if video == "Walk Back":
+        X = [0,0,0,0,0,0,0,0,0,1,start_frame,end_frame,lag]
+    st.success(fd)
+    st.success(ftd)
+    X = np.array(X)
+    X = X.reshape(1,-1)
+    linearY_pred = linearRegressor.predict(X)
+    rfY_pred = rfRegressor.predict(X)
+    LRP = "Linear Regression Prediction : " + str(linearY_pred[0])
+    st.success(LRP)
+    RFRP = "Random Forest Regression Prediction : " + str(rfY_pred[0])
+    st.success(RFRP)
